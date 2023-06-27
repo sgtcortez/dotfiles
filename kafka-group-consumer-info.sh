6eee65280d0cc4d4224c148435d09132c801b5bd
Matheus Rambo <matheusrambo24@gmail.com>
2023-06-27 14:32:51 -0300

    chore: Echo environment variables

--- kafka-group-consumer-info.sh
+++ kafka-group-consumer-info.sh
@@ -3,5 +3,10 @@
 # export KAFKA_CONSUMER_GROUP=your consumer group  
 # export KAFKA_ENVIRONMENTS=your kafka environemnts
 
+echo "Consumer: ${KAFKA_CONSUMER_GROUP}"   
+echo "Servers: ${KAFKA_ENVIRONMENTS}"
+
+echo "--------------------------------------"
+
 str="./kafka-consumer-groups.sh --group ${KAFKA_CONSUMER_GROUP} --bootstrap-server ${KAFKA_ENVIRONMENTS} --describe | awk ' { if(NR>2) printf(\"Lag: %10s, Producer Offset: %10s, Topic: %s\n\", \$6, \$5, \$2) }'"
 eval $str
